c5c2b1fe2a334e7f33802efb7883046d9bd6958f
ddd127 <demintsievd@yandex.ru>
2022-12-05 05:48:04 +0100

    Decrease number of coroutines

--- quicksort/src/main/java/org/example/quicksort/parallel/ParallelQuicksort.kt
+++ quicksort/src/main/java/org/example/quicksort/parallel/ParallelQuicksort.kt
@@ -1,6 +1,7 @@
 package org.example.quicksort.parallel
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
@@ -19,29 +20,36 @@ class ParallelQuicksort(
             withContext(dispatcher) {
                 quicksort(
                     arr = array,
-                    l = 0,
-                    r = array.size,
+                    lArg = 0,
+                    rArg = array.size,
                     block = array.size / (CORE_COUNT * MULTIPLIER) + 1,
                 )
             }
         }
     }
 
-    private suspend fun quicksort(arr: IntArray, l: Int, r: Int, block: Int) {
+    private suspend fun quicksort(arr: IntArray, lArg: Int, rArg: Int, block: Int) {
+        var l = lArg
+        var r = rArg
         coroutineScope {
-            if (r - l <= block) {
-                SequentialQuicksort.quickSort(arr, l, r)
-                return@coroutineScope
+            val createdJobs = mutableListOf<Job>()
+            while (r - l > block) {
+                val splitter = Helper.splitter(arr, l, r)
+                val (less, equal) = Helper.partition(arr, l, r, splitter)
+                if (less - l < r - equal) {
+                    createdJobs.add(
+                        launch { quicksort(arr, l, less, block) }
+                    )
+                    l = equal
+                } else {
+                    createdJobs.add(
+                        launch { quicksort(arr, equal, r, block) }
+                    )
+                    r = less
+                }
             }
-            val splitter = Helper.splitter(arr, l, r)
-            val (less, equal) = Helper.partition(arr, l, r, splitter)
-            val left = launch {
-                quicksort(arr, l, less, block)
-            }
-            val right = launch {
-                quicksort(arr, equal, r, block)
-            }
-            joinAll(left, right)
+            SequentialQuicksort.quickSort(arr, l, r)
+            createdJobs.joinAll()
         }
     }
 
