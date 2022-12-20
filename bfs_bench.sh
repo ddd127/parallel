4e7f5ff4129399a9f02efe1f39336f985a27dd75
ddd127 <demintsievd@yandex.ru>
2022-12-20 22:22:15 +0100

    Update bfs_bench.sh

--- bfs_bench.sh
+++ bfs_bench.sh
@@ -1,3 +1,3 @@
 git pull --rebase
 mvn clean install
-sudo nice -n 20 java -Xmx4g -Xms4g -XX:+UseZGC "$@" -jar bfs-benchmark/target/bfs-benchmarks.jar 2>&1 | tee out.log
+sudo nice -n 20 java -Xmx12g -Xms12g -XX:+UseG1GC "$@" -jar bfs-benchmark/target/bfs-benchmarks.jar 2>&1 | tee out.log
